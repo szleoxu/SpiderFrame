0295a32ee5d232eb2408b3bdc17b4521d2b4926b
xuliang <xuliang1@globalegrow.com>
2018-11-20 14:48:32 +0800

    Optimize error msg for url file

--- common/url.go
+++ common/url.go
@@ -19,7 +19,6 @@ func UrlResponse(url string) (*goquery.Document){
 		os.Exit(1)
 	}
 	if response.StatusCode == 200 {
-		fmt.Println("Response is success:"+url)
 		dom, err := goquery.NewDocumentFromReader(response.Body)
 		if err != nil {
 			log.Fatalf("Error msg: %s", err.Error())
@@ -39,7 +38,10 @@ func GetResponse(url string) *http.Response {
 	client := &http.Client{}
 	request, _ := http.NewRequest("GET", url, nil)
 	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0")
-	response, _ := client.Do(request)
+	response, err := client.Do(request)
+	if err!=nil{
+		log.Fatalf("Url response fail: %s",err.Error())
+	}
 	return response
 }
 
